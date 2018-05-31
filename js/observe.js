5cae18617d7e14b24a49e776fb97eb24719d2f77
Sommer <sommer.huang@toursforfun.cn>
2018-05-31 15:35:09 +0800

    modify

--- js/observe.js
+++ js/observe.js
@@ -2,7 +2,7 @@
  * @Author: Sommer 
  * @Date: 2018-05-30 17:45:38 
  * @Last Modified by: Sommer
- * @Last Modified time: 2018-05-30 18:35:13
+ * @Last Modified time: 2018-05-31 15:34:48
  */
 
 //监控Sommer中data的变化
@@ -19,7 +19,7 @@ function definePro (data, key, val) {
         enumerable: true,
         configurable: true,
         get: function() {
-            //添加订阅者watcher到订阅者集合中
+            //添加订阅者watcher到订阅者集合中，JS的浏览器单线程特性，保证这个全局变量在同一个时间内只会有一个watcher使用
             if (Dep.target) {
                 dep.addSub(Dep.target);
             }
